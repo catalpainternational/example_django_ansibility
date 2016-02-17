f68d1910380b59efd9ce825ecf47e43324bcf4a8
wicher <wicher@gavagai.eu>
2016-02-17 17:32:44 +0900

    v1 of the site

--- CattlePainterNational/paintjobs/views.py
+++ CattlePainterNational/paintjobs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Paintjob
 
 # Create your views here.
+def index(request):
+    job_list = Paintjob.objects.order_by('-due_date')
+    template = loader.get_template('paintjobs/index.html')
+    context = {
+        'job_list': job_list,
+    }
+    return HttpResponse(template.render(context, request))
