d6d1a105d48ec6470c9b428cfe2547c50f8f393b
boerwastaken <cat@gavagai.eu>
2022-02-11 09:28:22 +0100

    port to python3 & django3.2

--- CattlePainterNational/CattlePainterNational/settings.py
+++ CattlePainterNational/CattlePainterNational/settings.py
@@ -40,15 +40,14 @@ INSTALLED_APPS = (
     'paintjobs',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'CattlePainterNational.urls'
@@ -82,6 +81,7 @@ DATABASES = {
     }
 }
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
